0164c9e4bd779a6184b44e7e1471aa3c7f36b7f7
rizkifatihah <m.rizkifatihah@gmail.com>
2022-05-29 22:55:47 +0700

    build: V.1.9 Production

--- App.js
+++ App.js
@@ -624,7 +624,7 @@ export default function App() {
     let response = await request.json();
 
     if(response.buildnumber.toString()!==Application.nativeBuildVersion.toString()){
-      setIsNeedUpdate(false);
+      setIsNeedUpdate(true);
       setChangelog(response.changelog_mobile);
     }
 
